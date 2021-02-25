3e41814a66df8d674531c181294ba4b7eb46b81a
dassd05 <shrest@icloud.com>
2021-02-25 14:51:27 -0600

    tutorial explanations - shrest

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverRelativeControls.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverRelativeControls.java
@@ -100,18 +100,30 @@ public class DriverRelativeControls extends LinearOpMode {
             //driveVertical = -gamepad1.right_stick_y;
             //driveHorizontal = gamepad1.right_stick_x;
 
-            gamepadXCoordinate = gamepad1.right_stick_x;
-            gamepadYCoordinate = -gamepad1.right_stick_y;
+            gamepadXCoordinate = gamepad1.right_stick_x; //this simply gives our x value relative to the driver
+            gamepadYCoordinate = -gamepad1.right_stick_y; //this simply gives our y vaue relative to the driver
             gamepadHypot = Range.clip(Math.hypot(gamepadXCoordinate, gamepadYCoordinate), 0, 1);
+            //finds just how much power to give the robot based on how much x and y given by gamepad
+            //range.clip helps us keep our power within positive 1
+            // also helps set maximum possible value of 1/sqrt(2) for x and y controls if at a 45 degree angle (which yields greatest possible value for y+x)
             gamepadDegree = Math.atan2(gamepadYCoordinate, gamepadXCoordinate);
+            //the inverse tangent of opposite/adjacent gives us our gamepad degree
             robotDegree = getAngle();
+            //gives us the angle our robot is at
             movementDegree = gamepadDegree - robotDegree;
+            //adjust the angle we need to move at by finding needed movement degree based on gamepad and robot angles
             gamepadXControl = Math.cos(Math.toRadians(movementDegree)) * gamepadHypot;
+            //by finding the adjacent side, we can get our needed x value to power our motors
             gamepadYControl = Math.sin(Math.toRadians(movementDegree)) * gamepadHypot;
+            //by finding the opposite side, we can get our needed y value to power our motors
 
             /**
              * again, make sure you've changed the motor names and variables to fit your team
              */
+
+            //by mulitplying the gamepadYControl and gamepadXControl by their respective absolute values, we can guarantee that our motor powers will not exceed 1 without any driveTurn
+            //since we've maxed out our hypot at 1, the greatest possible value of x+y is (1/sqrt(2)) + (1/sqrt(2)) = sqrt(2)
+            //since (1/sqrt(2))^2 = 1/2 = .5, we know that we will not exceed a power of 1 (with no turn), giving us more precision for our driving
             frontRight.setPower(gamepadYControl * Math.abs(gamepadYControl) - gamepadXControl * Math.abs(gamepadXControl) + driveTurn);
             backRight.setPower(gamepadYControl * Math.abs(gamepadYControl) + gamepadXControl * Math.abs(gamepadXControl) + driveTurn);
             frontLeft.setPower(gamepadYControl * Math.abs(gamepadYControl) + gamepadXControl * Math.abs(gamepadXControl) - driveTurn);
